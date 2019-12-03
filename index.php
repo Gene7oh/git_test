f6d984d4d0631e7408ae7438c60e8215d1359d03
Gene7oh <gene7oh@hotmail.com>
2019-12-03 15:56:22 -0600

    Edit index create Class and init in the main col.

--- index.php
+++ index.php
@@ -1,5 +1,17 @@
 <?php
+$text = "The first dynamic text issued by PHP" . "<br>";
+class Bot
+{
+    var $track = "Tracking" . "<br>";
+    var $found = "Have been located " . "<br>";
+    var $missing = "Unable to located " . "<br>";
 
+    function bot_details(){
+        return "This app is " . $this->track . " you, so be careful. 
+        You will find you " . $this->found . " and ideally you should be " 
+        . $this->missing . "!";
+    }
+}
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -14,7 +26,11 @@
         <div class="row">
         <h1>Odin Project<small>Test Git</small></h1>
             <div class="col-8">
-            main div
+            <?php
+            echo $text;
+            $bot = new Bot();
+            echo $bot->bot_details();
+            ?>
             </div>
             <div class="col-4">
             side bar col
